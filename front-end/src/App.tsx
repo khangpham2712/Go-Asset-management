5abb83a59a73c53a2403c0a8e263e573dee41c8a
ngoc17to <ngoc81nh@gmail.com>
2023-11-04 13:33:55 +0700

    add ant

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from 'antd';
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to.
         </p>
+        return <DatePicker />;
         <a
           className="App-link"
           href="https://reactjs.org"
