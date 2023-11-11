73e93b12f01ee38b93d9b8b59c246cc35ea23a89
Nguyễn Thành An <nguyenthanhan140701@gmail.com>
2023-11-11 08:32:30 +0700

    .
    .

--- front-end/src/page/AddDepartment/add-department.tsx
+++ front-end/src/page/AddDepartment/add-department.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+
 import "../AddDepartment/add-department.css";
 import { Input } from 'antd';
 import { Button, Flex } from 'antd';
@@ -9,9 +11,26 @@ import { CloseOutlined } from '@ant-design/icons';
 // Tạo một functional component đơn giản
 const AddDepartment = () => {
 
+
+  const [departmentData, setDepartmentData] = useState({
+    departmentName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+
+
   const handleClose = () => {
     // Handle the close logic here
   };
+
+  const handleConfirm = () => {
+    // Handle the confirm logic here
+    console.log('Department Data:', departmentData);
+  };
+
+  
 
   return (
     <div className='addDeBox'>
@@ -30,12 +49,12 @@ const AddDepartment = () => {
 
         <div className='fillbox'>
         <div className='fill-box-text'>Password</div>
-        <Input placeholder="Please enter password"className='Input'  />
+        <Input placeholder="Please enter password"className='Input'type="password"  />
         </div>
 
         <div className='fillbox'>
         <div className='fill-box-text'>Comfirm password</div>
-        <Input placeholder="Please enter password"className='Input'  />
+        <Input placeholder="Please enter password"className='Input' type="password" />
         </div>
         
         <Button className='button-confirm' type="primary" >Comfirm</Button>
